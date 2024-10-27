edf5ce7ad7e3ccb679d9902ccbaf206d24ed52ef
Gustavo Jiménez <ttavojimenez26@gmail.com>
2024-10-26 19:36:27 -0500

    Game Manager , Dead zone

--- ArkanoidProject/Assets/Scripts/BallPhysics.cs
+++ ArkanoidProject/Assets/Scripts/BallPhysics.cs
@@ -19,4 +19,12 @@ public class BallPhysics : MonoBehaviour
         // Directly assign the velocity to the Rigidbody2D
         rigidBody2D.velocity = velocity;
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("GameOver"))
+        {
+            FindObjectOfType<GameManager>().LoseLife();
+        }
+    }
 }
